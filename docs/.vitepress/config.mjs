287c32e67a781a132a174156225419b3bd279da5
thomas.wang <thomas.wang@klook.com>
2024-04-09 19:05:04 +0800

    feat: 添加code snippet文件

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,27 +2,28 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Thomas前端小技巧合集",
+  lang: 'zh-CN',
+  title: "Thomas的笔记本",
+  lastUpdated: true,
   description: "用来记录工作中遇到的一些知识点或填坑小技巧",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    outlineTitle: '目录',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/web-wangle/thomas-vitepress-blog' }
     ]
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -5,20 +5,21 @@ hero:
   name: "Thomas小技巧合集"
   text: ""
   tagline: 用来记录工作中遇到的一些知识点或填坑小技巧
-  actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+  # actions:
+  #   - theme: brand
+  #     text: Markdown Examples
+  #     link: /markdown-examples
+  #   - theme: alt
+  #     text: API Examples
+  #     link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 常用代码段合集
+    details: 一些工作中经常用到的代码段合集
+    link: /code-snippet/index
+  - title: 敬请期待
+    details: 
+  - title: 敬请期待
+    details: 
 ---
 
